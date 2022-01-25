f1ec0efa6881d3ca7168baf42fbf6b2ec9183ea6
dhacqua <dylan.hacquart91@gmail.com>
2022-01-25 16:51:07 +0100

    Importation de la classe Database

--- app/src/main/java/com/example/movinder_project/MainActivity.java
+++ app/src/main/java/com/example/movinder_project/MainActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.movinder_project.Model.Database;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //touchez à vos fesses
+        Database db = new Database();
     }
 
 }

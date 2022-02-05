7369cfc9439252dc7e0419b82c043b944b7c6e02
Clément Legros <71426481+ClementLegros@users.noreply.github.com>
2022-02-05 12:01:12 +0100

    Avancement sur Register User

--- app/src/main/java/com/example/movinder_project/Model/UtilisateurPlaceHolderAPI.java
+++ app/src/main/java/com/example/movinder_project/Model/UtilisateurPlaceHolderAPI.java
@@ -1,8 +1,13 @@
 package com.example.movinder_project.Model;
 
 import java.util.List;
+
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface UtilisateurPlaceHolderAPI {
@@ -11,4 +16,9 @@ public interface UtilisateurPlaceHolderAPI {
 
     @GET("/login/{login}/{password}")
     Call<List<Utilisateur>> getUser(@Path(value="login", encoded=false) String login, @Path(value="password", encoded=false) String password);
+
+    @FormUrlEncoded
+    @POST("/registerUser")
+    Call<ResponseBody> registerUser(@Field("nom") String nom, @Field("email") String email, @Field("num") String num, @Field("mdp") String mdp, @Field("sexe") String sexe, @Field("orientation") String orientation);
+
 }
--- app/src/main/java/com/example/movinder_project/Register.java
+++ app/src/main/java/com/example/movinder_project/Register.java
@@ -2,15 +2,78 @@ package com.example.movinder_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.movinder_project.Model.Utilisateur;
+import com.example.movinder_project.Model.UtilisateurPlaceHolderAPI;
+
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Register extends AppCompatActivity {
+
+    private UtilisateurPlaceHolderAPI myUtilisayeurPlaceHolderAPI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void toRegister(View view) {
+
+        /* A COMPLETER */
+
+        EditText editTextNom = findViewById(R.id.editTextNom);
+        EditText editTextPassword = findViewById(R.id.userPassword);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextTel = findViewById(R.id.editTextPhone);
+
+        /* A COMPLETER */
+
+        String nom = editTextNom.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String email = editTextEmail.getText().toString();
+        String tel = editTextTel.getText().toString();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://themovinder.herokuapp.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        myUtilisayeurPlaceHolderAPI = retrofit.create(UtilisateurPlaceHolderAPI.class);
+
+        //Lié la methode registerAPI
+    }
+
+    public void registerAPI(String nom, String password, String email, String num, String sexe, String orientation)
+    {
+        Call<ResponseBody> call = myUtilisayeurPlaceHolderAPI.registerUser(nom, email, num, password, sexe, orientation);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    Log.d("API", "Register Success");
+                } else {
+                    Log.d("API", "Register Failed");
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.d("API", "The request API as a problem");
+            }
+        });
     }
 
     //test commit
